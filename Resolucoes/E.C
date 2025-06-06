7f72687bbd35d51fc9f8f9edcd558d15629270b9
na1tto <eduardo3245.ss@gmail.com>
2025-06-05 23:09:40 -0300

    Res. Letra E SBC Fase Zero

--- Resolucoes/E.C
+++ Resolucoes/E.C
@@ -2,21 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int calcularMDC(int a, int b) {
-    if (b == 0) {
-        return a;
+int gcd(int a, int b)
+{
+    // Find Minimum of a and b
+    int result = ((a < b) ? a : b);
+    while (result > 0) {
+        if (a % result == 0 && b % result == 0) {
+            break;
+        }
+        result--;
     }
-    return calcularMDC(b, a % b);
+
+    // Return gcd of a and b
+    return result;
 }
 
 int main(){
     int x = 1;
-    int y;
-    int k;
+    int y = 0;
+    int k = 0;
     scanf("%d", &y);	
     scanf("%d", &k);
     for(int i = 0; i < k; i++){
-        int mdc = calcularMDC(x,y);
+        int mdc = gcd(x,y);
         x = x + mdc; 
     }
     printf("%d\n", x);
